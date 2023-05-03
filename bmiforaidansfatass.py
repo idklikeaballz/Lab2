211e60b74802b783c2f32b2bada4b3a31d5e8e25
Shaun <shaunboo.22@ichat.sp.edu.sg>
2023-05-03 09:50:47 +0800

    updated like a fool

--- bmiforaidansfatass.py
+++ bmiforaidansfatass.py
@@ -11,10 +11,15 @@ def calculate_bmi (height, weight):
     print (bmi)
     if bmi<18.5:
         print ("Under Weight")
+        return -1
+
+
     elif 18.5<=bmi<=25.0:
             print ("Normal Weight")
+            return 0
     elif bmi>25.0:
             print("Overweight")
+            return 1
 
 
 calculate_bmi(weight=57,height=1.73)
